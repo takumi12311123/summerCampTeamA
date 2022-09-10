312092da4d5affee8704574c9738f3c82008cdd0
Takuaka1231 <takumiakasaka1231@gmail.com>
2022-09-10 15:50:21 +0900

    calling_oneの作成

--- app/api/turnOnVideo.js
+++ app/api/turnOnVideo.js
@@ -2,7 +2,6 @@ import Peer from "skyway-js";
 
 const peer = new Peer({ key: process.env.API_KEY, debug: 3 });
 
-function yuki(){
-    const a = "aaa"
-AudioParamMap
+export function getPeerId() {
+  return peer;
 }
