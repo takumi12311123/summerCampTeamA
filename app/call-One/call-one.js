e72555f476963f5eb97176b5057dd0f4f2cb5839
Takuaka1231 <takumiakasaka1231@gmail.com>
2022-09-11 16:01:01 +0900

    call-one.jsの編集途中まで

--- app/call-One/call-one.js
+++ app/call-One/call-one.js
@@ -1,8 +1,26 @@
 const Peer = window.Peer;
-const remoteStream = getElementById("js-remote-stream")
-const localStream = getElementById("js-local-stream")
-const localId = getElementById("js-local-id")
-const remoteId = getElementById("js-remote-id")
-const callTrigger = getElementById("js-call-trigger")
-const closeTrigger = getElementById("js-close-trigger")
-const meta = getElementById("js-meta")
+const remoteStream = getElementById("js-remote-stream");
+const localStream = getElementById("js-local-stream");
+const remoteId = getElementById("js-remote-id");
+const callTrigger = getElementById("js-call-trigger");
+const closeTrigger = getElementById("js-close-trigger");
+const meta = getElementById("js-meta");
+
+const peer = (window.peer = new Peer({
+  key: window.__SKYWAY_KEY__,
+  debug: 3,
+}));
+
+callTrigger.addEventListener("click", () => {
+  if (!peer.open) {
+    return;
+  }
+  const localId = getElementById("js-local-id");
+  const call = peer.call(localId.value, localStream);
+  closeTrigger.addEventListener("click", () => mediaConnection.close(true));
+});
+
+peer.on("error", (error) => {
+  console.log(`${error.type}: ${error.message}`);
+  // => room-error: Room name must be defined.
+});
